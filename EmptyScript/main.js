ee59ecec61f971446c1052f1f3a9a61ede4bcefa
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2020-12-04 20:56:59 -0500

    Init

--- EmptyScript/main.js
+++ EmptyScript/main.js
@@ -2,7 +2,28 @@ load.initialize(async function () {
 });
 
 load.action("Action", async function () {
-    load.log("Hello Azure Pipelines");
+    load.WebRequest.defaults.returnBody = false;
+    load.WebRequest.defaults.headers = {
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "en-US,en;q=0.9",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
+    };
+
+    let T01 = new load.Transaction("T00_HomePage");
+
+    T01.start();
+
+    new load.WebRequest({
+        id: 1,
+        url: "http://www.example.com/",
+        method: "GET",
+        handleHTTPError: (webResponse) => {
+            if (webResponse.status === 404) {
+                return false;
+            }
+        }
+    }).sendSync();
+    T01.stop();
 });
 
 load.finalize(async function () {
